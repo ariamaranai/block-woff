5a4288d9f2e283d13f1fb7d5f9382cafe5147e4f
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-04-15 06:14:06 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,9 +1,7 @@
 chrome.action.onClicked.addListener(async () => {
   let len = (await chrome.declarativeNetRequest.getEnabledRulesets()).length;
-  chrome.action.setIcon({ path: len ? "off.png": "on.png" }),
-  chrome.declarativeNetRequest.updateEnabledRulesets({
-    [len ? "disableRulesetIds" : "enableRulesetIds"] : ["0"]
-  });
+  chrome.action.setIcon({ path: len ? "off.png": "on.png" });
+  chrome.declarativeNetRequest.updateEnabledRulesets({ [len ? "disableRulesetIds" : "enableRulesetIds"] : ["0"] });
 });
 chrome.runtime.onStartup.addListener(async () =>
   chrome.action.setIcon({ path: (await chrome.declarativeNetRequest.getEnabledRulesets()).length ? "on.png" : "off.png" })
